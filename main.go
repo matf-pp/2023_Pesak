828077e41c1e6f4b3fd7b04c9067e8a221a6ae3a
Spera97 <mn16259@alas.matf.bg.ac.rs>
2023-04-21 20:42:56 +0200

    prebacio parce koda u drugi fajl

--- main.go
+++ main.go
@@ -3,40 +3,25 @@ package main
 import 
 (
 	"github.com/veandco/go-sdl2/sdl"
-	"math/rand"
+//	"math/rand"
 	"math"
 //	"time"
 	"fmt"
-//	"app/mat"
+	"main/mat"
 )
 
-type Materijal int
-
-const (
-	Zid Materijal = -1
-	Prazno Materijal = 0
-	Pesak Materijal = 1
-	Voda Materijal = 2
-	Metal Materijal = 3
-	//itd
-)
-
-var boja = map[Materijal]uint32{
-	Zid : 0xffffff,
-	Prazno : 0x000000,
-	Pesak : 0xffff66,
-	Voda : 0x3333ff,
-	Metal : 0x33334b,
-//	Kamen : 0x666666,
-}
+var boja = mat.Boja
 
 const sirinaKanvasa, visinaKanvasa = 240, 144
-const brojPikselaPoCestici = 4
-//golang nema makroe pa koristimo globalne konst (za sada?) -s
+//const brojPikselaPoCestici = 4
+const brojPikselaPoCestici = 8
+//TODO automatska detekcija rezolucije ekrana
 
-var trenutniMat Materijal = Pesak
-var velicinaKursora int32 = 0
-var maxKursor int32 = 8
+var trenutniMat mat.Materijal = mat.Pesak
+//var velicinaKursora int32 = 0
+var velicinaKursora int32 = 4
+var maxKursor int32 = 32
+var pause bool = true
 
 var keystates = sdl.GetKeyboardState()
 
@@ -60,23 +45,27 @@ func main() {
 	}
 	surface.FillRect(nil, 0)
 
-	var matrix[sirinaKanvasa][visinaKanvasa]Materijal
+	var matrix[sirinaKanvasa][visinaKanvasa] mat.Cestica
 	slajs := matrixToSlice(matrix)
+	var bafer[sirinaKanvasa][visinaKanvasa] mat.Cestica
+	bajs := matrixToSlice(bafer)
 
-	//TODO izdvojiti ove dve ruzne petlje van mejna? `void zidaj(slajs);` npr -s
+	//TODO izdvojiti ove dve ruzne petlje van mejna? `void mat.Zidaj(slajs);` npr -s
 	for i := 0; i < sirinaKanvasa; i++ {
-		slajs[i][0] = Zid
-		slajs[i][visinaKanvasa-1] = Zid
+		slajs[i][0] = mat.NewCestica(mat.Zid)
+		slajs[i][visinaKanvasa-1] = mat.NewCestica(mat.Zid)
 	}
 	for j := 0; j < visinaKanvasa; j++ {
-		slajs[0][j] = Zid
-		slajs[sirinaKanvasa-1][j] = Zid
+		slajs[0][j] = mat.NewCestica(mat.Zid)
+		slajs[sirinaKanvasa-1][j] = mat.NewCestica(mat.Zid)
 	}
 
 	running := true
 	for running {
-		running = pollEvents(slajs)
-		update(slajs)
+		running = pollEvents(slajs, bajs)
+		if !pause {
+			updateCanvas(slajs, bajs)
+		}
 		render(slajs, surface)
 
 		window.UpdateSurface()
@@ -87,26 +76,32 @@ func main() {
 func clampCoords(x int32, y int32) (int32, int32) {
 	//osigurava da tacka ne izleti iz ekrana sto se desava u raznim slucajevima -s
 	// radi isto kao što piše iznad, samo u jednoj liniji /limun
+	// lep funkcionalni pristup, kolega -s
 	return int32(math.Min(math.Max(float64(x), 0), sirinaKanvasa - 1)), int32(math.Min(math.Max(float64(y), 0), visinaKanvasa - 1))
 }
 
-func brush(matrix [][]Materijal, x int32, y int32, state uint32) {
+func brush(matrix [][]mat.Cestica, bafer [][]mat.Cestica, x int32, y int32, state uint32) {
+	//TODO za srednji klik da uzme materijal na koj mis trenutno pokazuje i postavi ga kao trenutni
+	//ukoliko nije u pitanju Zid ili Prazno. Nije mi pri ruci mis, mrzi me da trazim koj je to stejt -s
 	// zamenio redosled if-a i for-a /limun
-	// dole se povećavala veličina kursora za 2, a ovde delila sa 2, pa sam sklonio /2 i +2 na +1 /limun
+	// dole se povećavala veličina kursora za 2, a ovde delila sa 2, pa sam sklonio /2 i +2 na +1 /limun 
 	for i := -velicinaKursora; i <= velicinaKursora; i++ {
-		for j := -velicinaKursora; j <= velicinaKursora; j++{
+		for j := -velicinaKursora; j <= velicinaKursora; j++ {
 			tx, ty := clampCoords(x/brojPikselaPoCestici+i, y/brojPikselaPoCestici+j)
-			if state == 1 && matrix[tx][ty] == Prazno {
-				matrix[tx][ty] = trenutniMat
+			if state == 1 && matrix[tx][ty].Materijal == mat.Prazno {
+				matrix[tx][ty] = mat.NewCestica(trenutniMat)
+				bafer[tx][ty] = matrix[tx][ty]
+
 			}
-			if state == 4 && matrix[tx][ty] != Zid {
-				matrix[tx][ty] = Prazno
+			if (state == 4 || (state == 1 && trenutniMat == mat.Prazno)) && matrix[tx][ty].Materijal != mat.Zid && matrix[tx][ty].Materijal!= mat.Prazno{
+				matrix[tx][ty] = mat.NewCestica(mat.Prazno)
+				bafer[tx][ty] = matrix[tx][ty]
 			}
 		}
 	}
 }
 
-func pollEvents(matrix [][]Materijal) bool {
+func pollEvents(matrix [][]mat.Cestica, bafer [][]mat.Cestica) bool {
 	running := true
 	keystates = sdl.GetKeyboardState()
 
@@ -118,26 +113,41 @@ func pollEvents(matrix [][]Materijal) bool {
 			case *sdl.KeyboardEvent:
 				// može ovo lepše #TODO
 				// kako si to zamisljao lepse? -s
+				// ignorisite ovo, mislim da pricam sam sa sobom (nz ciji je prvi kom)
 				if keystates[sdl.SCANCODE_ESCAPE] != 0 {
 					running = false
 				}
 				if keystates[sdl.SCANCODE_0] != 0 {
-					trenutniMat = Prazno
+					trenutniMat = mat.Prazno
 				}
 				if keystates[sdl.SCANCODE_1] != 0 {
-					trenutniMat = Pesak
+					trenutniMat = mat.Pesak
 				}
 				if keystates[sdl.SCANCODE_2] != 0 {
-					trenutniMat = Voda
+					trenutniMat = mat.Voda
 				}
 				if keystates[sdl.SCANCODE_3] != 0 {
-					trenutniMat = Metal
+					trenutniMat = mat.Metal
+				}
+				if keystates[sdl.SCANCODE_4] != 0 {
+					trenutniMat = mat.Kamen
+				}
+				if keystates[sdl.SCANCODE_5] != 0 {
+					trenutniMat = mat.Lava
+				}
+				if keystates[sdl.SCANCODE_6] != 0 {
+					trenutniMat = mat.Led
+				}
+				if keystates[sdl.SCANCODE_7] != 0 {
+					trenutniMat = mat.Para
 				}
 				if keystates[sdl.SCANCODE_DOWN] != 0 {
-					// povećavalo se za 2, a onda delilo sa 2 u petlji gore, pa sam smanjio na -1 i skinuo /2 /limun
 					if velicinaKursora > 0 {
 						velicinaKursora = velicinaKursora - 1
 					}
+				}
+				if keystates[sdl.SCANCODE_P] != 0 {
+					pause = !pause
 				}
 				if keystates[sdl.SCANCODE_UP] != 0 {
 					if velicinaKursora < maxKursor {
@@ -157,24 +167,27 @@ func pollEvents(matrix [][]Materijal) bool {
 	fmt.Printf("xpx: %d\t", x/brojPikselaPoCestici)
 	fmt.Printf("ypx: %d\t", y/brojPikselaPoCestici)
 	fmt.Printf("mb: %d\t", state)
-	fmt.Printf("materijal: %d\t", trenutniMat)
-	fmt.Printf("velicina: %d\n", velicinaKursora)
+	fmt.Printf("mat.Materijal: %d\t", trenutniMat)
+	fmt.Printf("velicina: %d\t", velicinaKursora)
+	fmt.Printf("pauza: %t\n", pause)
 
-	brush(matrix, x, y, state)	
+	brush(matrix, bafer, x, y, state)	
 
 	return running
 
 }
 
-func update(matrix [][]Materijal) {
-	for j := visinaKanvasa-3; j > 0; j-- {
-		for i := 1; i < sirinaKanvasa-1; i++ {
+func updateCanvas(matrix [][]mat.Cestica, bafer [][]mat.Cestica) {
+	for j := 1; j < visinaKanvasa-2; j++ {
+		for i := 1; i < sirinaKanvasa-2; i++ {
+			mat.Update(matrix, bafer, i, j)
 
-			// pesak
-			if matrix[i][j] == Pesak {
+
+/**			// mat.Pesak
+			if matrix[i][j] == mat.Pesak {
 				//flutter := rand.Intn(2)
 				//predlazem da lelujanje ostavimo za malo kasnije da pojednostavimo ovo dok ne sredimo lepo -s
-				if matrix[i][j+1] == Prazno || matrix[i][j+1] == Voda{
+				if matrix[i][j+1] == mat.Prazno || matrix[i][j+1] == mat.Voda{
 //					if flutter == 1 {
 						matrix[i][j], matrix[i][j+1] = matrix[i][j+1], matrix[i][j]
 //					}//	ovde prvi put a nadalje puno puta postaje ocigledna potreba za nekom swap
@@ -188,57 +201,63 @@ func update(matrix [][]Materijal) {
 						sgn = -1
 					}//nasumice biramo na koju stranu prvo ide da izbegnemo pristrasno padanje
 
-					if (matrix[i+sgn][j+1] == Prazno || matrix[i+sgn][j+1] == Voda) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
+					if (matrix[i+sgn][j+1] == mat.Prazno || matrix[i+sgn][j+1] == mat.Voda) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
 						matrix[i][j], matrix[i+sgn][j+1] = matrix[i+sgn][j+1], matrix[i][j]
-					} else if (matrix[i-sgn][j+1] == Prazno || matrix[i-sgn][j+1] == Voda) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
+					} else if (matrix[i-sgn][j+1] == mat.Prazno || matrix[i-sgn][j+1] == mat.Voda) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
 						matrix[i][j], matrix[i-sgn][j+1] = matrix[i-sgn][j+1], matrix[i][j]
 					} 
 				}
 			}
-			// voda
+			// mat.Voda
 			// ovo je teže nego što smo mislili
 			//mozda teze nego sto si /ti/ mislio -s
-			if matrix[i][j] == Voda {
+			if matrix[i][j] == mat.Voda {
 //				flutter := rand.Intn(2)
-				if matrix[i][j+1] == Prazno {
+				if matrix[i][j+1] == mat.Prazno {
 //					if flutter == 1 {
-						matrix[i][j], matrix[i][j+1] = Prazno, Voda
+						matrix[i][j], matrix[i][j+1] = mat.Prazno, mat.Voda
 //					}
 				} else {
 					var sgn int = rand.Intn(3)
 					sgn = sgn - 1
 					// -1, 0, 1
 
-					if (matrix[i+sgn][j+1] == Prazno) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
-						matrix[i][j], matrix[i+sgn][j+1] = Prazno, Voda
-					} else if (matrix[i-sgn][j+1] == Prazno) && (i-sgn > 0) && (i-sgn < sirinaKanvasa) {
-						matrix[i][j], matrix[i-sgn][j+1] = Prazno, Voda
-					} else if (matrix[i+sgn][j] == Prazno) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
-						matrix[i][j], matrix[i+sgn][j] = Prazno, Voda
-					} else if (matrix[i-sgn][j] == Prazno) && (i-sgn > 0) && (i-sgn < sirinaKanvasa) {
-						matrix[i][j], matrix[i-sgn][j] = Prazno, Voda
+					if (matrix[i+sgn][j+1] == mat.Prazno) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
+						matrix[i][j], matrix[i+sgn][j+1] = mat.Prazno, mat.Voda
+					} else if (matrix[i-sgn][j+1] == mat.Prazno) && (i-sgn > 0) && (i-sgn < sirinaKanvasa) {
+						matrix[i][j], matrix[i-sgn][j+1] = mat.Prazno, mat.Voda
+					} else if (matrix[i+sgn][j] == mat.Prazno) && (i+sgn > 0) && (i+sgn < sirinaKanvasa) {
+						matrix[i][j], matrix[i+sgn][j] = mat.Prazno, mat.Voda
+					} else if (matrix[i-sgn][j] == mat.Prazno) && (i-sgn > 0) && (i-sgn < sirinaKanvasa) {
+						matrix[i][j], matrix[i-sgn][j] = mat.Prazno, mat.Voda
 					}
 				}
-			}
+			}/**/
+		}
+	}
+
+	for j := 1; j < visinaKanvasa-2; j++ {
+		for i := 1; i < sirinaKanvasa-2; i++ {
+			matrix[i][j] = bafer[i][j]
 		}
 	}
 }
 
-func render(matrix [][]Materijal, surface *sdl.Surface) {
+func render(matrix [][]mat.Cestica, surface *sdl.Surface) {
 	for i := 0; i < sirinaKanvasa; i++ {
 		for j := 0; j < visinaKanvasa; j++ {
 			rect := sdl.Rect{int32(i*brojPikselaPoCestici), int32(j*brojPikselaPoCestici), brojPikselaPoCestici,brojPikselaPoCestici}
-			surface.FillRect(&rect, boja[matrix[i][j]])
+			surface.FillRect(&rect, boja[matrix[i][j].Materijal])
 		}
 	}
 }
 
-func matrixToSlice(matrix [sirinaKanvasa][visinaKanvasa]Materijal) [][]Materijal {
+func matrixToSlice(matrix [sirinaKanvasa][visinaKanvasa]mat.Cestica) [][]mat.Cestica {
 
-	slajs := make([][]Materijal, len(matrix))
+	slajs := make([][]mat.Cestica, len(matrix))
 
 	for i := 0; i < len(matrix); i++ {
-		kolona := make([]Materijal, len(matrix[i]))
+		kolona := make([]mat.Cestica, len(matrix[i]))
 		for j := 0; j < len(matrix[i]); j++ {
 			kolona[j] = matrix[i][j]
 		}
--- mat/mat.go
+++ mat/mat.go
@@ -1,21 +1,215 @@
 package mat
 
+import
+(
+	"math"
+)
+
 type Materijal int
 
 const (
-    Zid Materijal = -1
-    Prazno Materijal = 0
-    Pesak Materijal = 1
-    Voda Materijal = 2
-    Kamen Materijal = 3
-    Metal Materijal = 4
+	Zid Materijal = 256
+	Prazno Materijal = 0
+	Pesak Materijal = 1
+	Voda Materijal = 2
+	Metal Materijal = 3
+	Kamen Materijal = 4
+	Lava Materijal = 5
+	Led Materijal = 6
+	Para Materijal = 7
 )
 
 var Boja = map[Materijal]uint32{
-    Zid : 0xffffff,
-    Prazno : 0x000000,
-    Pesak : 0xffff66,
-    Voda : 0x3333ff,
-    Kamen : 0x666666,
-    Metal : 0x33334b,
+	Zid : 0xffffff,
+	Prazno : 0x000000,
+	Pesak : 0xffff66,
+	Voda : 0x3333ff,
+	Metal : 0x33334b,
+	Kamen : 0x666666,
+	Lava : 0xff6600,
+	Led : 0xccccff,
+	Para : 0x9999cc,
 }
+
+var Gustina = map[Materijal]int32{
+	Zid : 0,
+	Prazno : 0,
+	Pesak : 5,
+	Voda : 3,
+	Metal : 0,
+	Kamen : 5,
+	Lava : 4,
+	Led : 0,
+	Para : -5,
+}
+
+//	0000 nece on nidje
+//	0001 pada direkt
+//	0010 pada dijagonalno
+//	0100 curi horizontalno
+var AStanje = map[Materijal]int{
+	Zid :		0b0000,
+	Prazno :	0b1111,
+	Pesak :		0b0011,
+	Voda :		0b0111,
+	Metal :		0b0000,
+	Kamen :		0b0001,
+	Lava :		0b0111,
+	Led :		0b0000,
+	Para :		0b0111,
+}
+
+var FaznaPromena = map[Materijal] [4]uint32{
+	//TODO pretvoriti [4]int u struct{Materijal, int, int,Materijal}
+	Zid : {uint32(Zid), 0, math.MaxUint32, uint32(Zid)},
+	Prazno : {uint32(Prazno), 0, math.MaxUint32, uint32(Zid)},
+	Pesak : {uint32(Pesak), 0, 1986, uint32(Lava)}, //1986
+	Voda : {uint32(Led), 273, 373, uint32(Para)},
+	Metal : {uint32(Metal), 0, 1811, uint32(Lava)}, //1811
+	Kamen : {uint32(Kamen), 0, 1473, uint32(Lava)}, //1473
+	Lava : {uint32(Lava), 1300, math.MaxUint32, uint32(Lava)},
+	Led : {uint32(Led), 0, 273, uint32(Voda)},
+	Para : {uint32(Voda), 373, math.MaxUint32, uint32(Para)},
+
+}
+
+var Zapaljiv = map[Materijal]bool{
+	Zid : false,
+	Prazno : false,
+	Pesak : false,
+	Voda : false,
+	Metal : false,
+	Kamen : false,
+	Lava : false,
+	Led : false,
+	Para : false,
+}
+
+
+/*	ako pozelimo da ga bas objektno krkamo to bi izgledalo ovako nekako
+type Materijali struct {
+	boja uint32
+	padajuc bool
+	tecan bool
+	tezina int
+	zapaljiv bool
+}
+
+type Pesakk struct {
+	Materijali
+	temperatura uint32
+}
+
+func noviPesakk() Pesakk {
+	zrno := Pesakk{
+		Materijali : Materijali{
+			boja : 0xffff66,
+			padajuc : true,
+			tecan : false,
+			tezina : 5,
+			zapaljiv : false,
+			},
+			temperatura : 293, //20 celzijusa
+	}
+	return zrno
+}
+*/
+
+type Cestica struct {
+
+	Materijal Materijal
+	Temperatura uint32
+	SekMat Materijal
+
+}
+
+func NewCestica(materijal Materijal) Cestica{
+	zrno := Cestica {
+		Materijal : materijal,
+		Temperatura : 293, //20 celzijusa
+		SekMat : Prazno,
+	}//ovaj deo if materijal je uzasno ruzan -s
+	if materijal == Led {
+		zrno.SekMat = Voda
+		zrno.Temperatura = 253 //-20
+	}
+	if materijal == Para {
+		zrno.SekMat = Voda
+		zrno.Temperatura = 383 //110
+	}
+	if materijal == Lava {
+		zrno.SekMat = Kamen
+		zrno.Temperatura = 1400
+	}
+	return zrno
+}
+
+//Update(matrix, bafer, i, j, matrix[i][j].Materijal)
+//nisam u stanju da procenim trenutno treba li ovo prebaciti u treci fajl ili Boga pitaj gde -s
+func Update(matrix [][]Cestica, bafer [][]Cestica, i int, j int) {
+
+	if matrix[i][j].Materijal == Prazno {
+		return
+	}
+
+	materijal := matrix[i][j].Materijal
+	temperatura := matrix[i][j].Temperatura
+//	sekmat := matrix[i][j].SekMat
+
+
+	//faza
+	if materijal != Lava{
+		if temperatura < FaznaPromena[materijal][1] {
+			matrix[i][j].Materijal = Materijal(FaznaPromena[materijal][1])
+			matrix[i][j].Temperatura = matrix[i][j].Temperatura
+			matrix[i][j].SekMat = materijal
+		} else if temperatura > FaznaPromena[materijal][2] {
+			matrix[i][j].Materijal = Materijal(FaznaPromena[materijal][3])
+			matrix[i][j].Temperatura = matrix[i][j].Temperatura
+			matrix[i][j].SekMat = materijal
+		}
+	} else {
+		if temperatura < FaznaPromena[materijal][1] {
+			matrix[i][j].Materijal = matrix[i][j].SekMat
+		}
+	}
+
+	//gorenje
+	if Zapaljiv[materijal] {
+		//TODO
+	}
+
+	//pomeranje ce morati biti redna petlja a ne paralelna sa ostalim efektima (zameni se pesak s naftom a tek onda nafta eksplodira i sta onda) -s
+	//padanje
+	pomeren := false
+	padanje := AStanje[materijal]
+	if (padanje & 0b0001) != 0 {
+		gornji := matrix[i][j-1]
+		gornjiMat := gornji.Materijal
+		donji := matrix[i][j+1]
+		donjiMat := donji.Materijal
+		if ((AStanje[gornjiMat] & 0b0001) != 0) && Gustina[gornjiMat] > Gustina[materijal] {
+			bafer[i][j].Materijal = bafer[i][j-1].Materijal
+			bafer[i][j].Temperatura = bafer[i][j-1].Temperatura
+			bafer[i][j].SekMat = bafer[i][j-1].SekMat
+			bafer[i][j-1].Materijal = bafer[i][j].Materijal
+			bafer[i][j-1].Temperatura = bafer[i][j].Temperatura
+			bafer[i][j-1].SekMat = bafer[i][j].SekMat
+			pomeren = true
+		} else if AStanje[donjiMat] != 0b0000 && Gustina[donjiMat] < Gustina[materijal] {
+			bafer[i][j].Materijal = bafer[i][j+1].Materijal
+			bafer[i][j].Temperatura = bafer[i][j+1].Temperatura
+			bafer[i][j].SekMat = bafer[i][j+1].SekMat
+			bafer[i][j+1].Materijal = bafer[i][j].Materijal
+			bafer[i][j+1].Temperatura = bafer[i][j].Temperatura
+			bafer[i][j+1].SekMat = bafer[i][j].SekMat
+			pomeren = true
+		}
+	}
+
+	if pomeren {
+		
+	}
+
+}
+
